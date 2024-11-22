4c55a58b432d4cf909d6232f3a3aa1eead40ccb8
sammy1-com <njugunas357@gmail.com>
2024-11-22 20:33:34 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    
+    if (username === 'admin' && password === 'admin') {
+        document.getElementById('message').textContent = 'Login successful!';
+        document.getElementById('message').style.color = 'green';
+    } else {
+        document.getElementById('message').textContent = 'Invalid username or password.';
+    }
+});
